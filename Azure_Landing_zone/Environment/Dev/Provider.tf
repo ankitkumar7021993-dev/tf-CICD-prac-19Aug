60cb17b95993e95756ee65bc0eadeef21c48ebb8
Ankit_Kumar <ankit.kumar7021993@gmail.com>
2026-08-22 19:13:11 +0530

    Update Provider.tf

--- Azure_Landing_zone/Environment/Dev/Provider.tf
+++ Azure_Landing_zone/Environment/Dev/Provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "registry.terraform.io/hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.80.0"
     }
   }
